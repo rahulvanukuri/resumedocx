85e99ab365748e181c9328280054b33898eb0eb5
rahulvanukuri62-prog <rahulvanukuri62@gmail.com>
2026-02-08 22:39:42 -0600

    Update project

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def generate_docx(payload: GenerateRequest, template_path: str):
 
         # Render with the *new* context
         doc.render(final_context)
-        doc.save("Rahul Vanukuri.docx")
+        doc.save("RahulVanukuri.docx")
 
         buffer = BytesIO()
         doc.save(buffer)
@@ -110,7 +110,7 @@ def generate_docx(payload: GenerateRequest, template_path: str):
         docx_b64 = base64.b64encode(buffer.read()).decode("utf-8")
 
         return GenerateResponse(
-            filename="Rahul Vanukuri.docx",
+            filename="RahulVanukuri.docx",
             docx_b64=docx_b64
         )
     
